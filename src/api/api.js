125aea8e5b5411e8d570608cb3027bc0c5fd8366
Diego_sdd <diegodiasantana@gmail.com>
2021-11-15 03:25:08 -0300

    -

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-import constants from '../config/constants';
+//import constants from '../config/constants';
 
 const api = axios.create({
-  baseURL: constants.generalApiPath,
+  baseURL: 'https://pi-project-univesp-back.herokuapp.com/',
 });
 
 export default api;
--- src/api/loginUser.js
+++ src/api/loginUser.js
@@ -2,14 +2,14 @@
 import api from './api';
 
 export async function LoginUser(data) {
-
+  console.log(api)
   try {
     const result = await api.get('/v1/loginUser',
       {
         params: data
       }
     );
-
+    console.log(result)
     return result;
   } catch (error) {
 
